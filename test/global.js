e989bb1a8a356227d5b8f06493abda34c78541a8
harlott <marcoallott@gmail.com>
2018-01-23 09:17:13 +0100

    clean code

--- test/global.js
+++ test/global.js
@@ -20,6 +20,5 @@ before(function() {
   global.__global__ = {}
   global.newStorage = buildCustomStorage('fileSystem', (p, v)=>{global.__global__[p]=v}, (p)=>{return global.__global__[p]}, (p)=>{global.__global__[p] = undefined})
   global.customStoragesMap = buildCustomStoragesMap('fileSystem', global.newStorage)
-  console.log(`global.customStoragesMap = ${JSON.stringify(global.customStoragesMap)}`)
   global.storage = new WebStorage('fileSystem', global.customStoragesMap, {enabled: true, grantedProps: ['a', 'b']})
 });
